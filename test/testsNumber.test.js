b57f42e2fe5a3f6491380ab137a229a33f2fc826
Maeve Griffin <griffinmaeve@gmail.com>
2019-07-17 10:48:14 -0700

    trying to get travit to calm tf down

--- test/testsNumber.test.js
+++ test/testsNumber.test.js
@@ -5,9 +5,9 @@ const test = QUnit.test;
 test('compair numbers', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-    const guess = 'banana';
+    const guess = 6;
     const correct = 6;
-    const expected = true;
+    const expected = 0;
     //Act 
     // Call the function you're testing and set the result to a const
     const result = numberGame(guess, correct);
