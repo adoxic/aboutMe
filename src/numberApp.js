dd314cc7d175f378823f97fb8706708f5d459868
Maeve Griffin <griffinmaeve@gmail.com>
2019-07-17 11:19:27 -0700

    fixed for real this time

--- src/numberApp.js
+++ src/numberApp.js
@@ -9,12 +9,10 @@ let trys = 4;
 
 
 //random integer syntax pulled from MDN https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(min, max) {
-    min = Math.ceil(1);
-    max = Math.floor(20);
-    return Math.floor(Math.random() * (20 - 1)) + 1; 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max - 1)) + 1; 
 }
-let randomNum = getRandomInt(1, 20); 
+let randomNum = getRandomInt(20); 
 
 
 button.addEventListener('click', () => {
