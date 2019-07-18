7c82267cf514ca686881f71dddd2d76a9f2cc880
Maeve Griffin <griffinmaeve@gmail.com>
2019-07-18 10:57:58 -0700

    TDD function to calculate player win stretch goal working

--- test/RPScomp.test.js
+++ test/RPScomp.test.js
@@ -1,17 +1,15 @@
 import rpsCompair from '../RPS src/RPScomp.js';
-import { backToNum } from '../RPS src/RPScomp.js';
-import compString from '../RPS src/RPS.js';
 
 const test = QUnit.test;
 
-let computer = backToNum(compString);
-let human = 2;
+
 
 test('who wins?', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-    
-    const expected = 'human wins';
+    let human = 1;
+    let computer = 'scissors2';
+    const expected = 2;
     //Act 
     // Call the function you're testing and set the result to a const
     const result = rpsCompair(human, computer);
