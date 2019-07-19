872aeaf5d8e539ddb9bdbbd3e65f19fa0e9542cd
Libby Hamlin <bravolibby@gmail.com>
2019-07-19 11:09:16 -0700

    pair partner changes

--- test/RPScomp.test.js
+++ test/RPScomp.test.js
@@ -1,4 +1,4 @@
-import rpsCompair from '../RPS src/RPScomp.js';
+import rpsCompair from '../rps-src/RPScomp.js';
 
 const test = QUnit.test;
 
@@ -8,11 +8,37 @@ test('who wins?', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     let human = 1;
-    let computer = 'scissors2';
+    let computer = 'scissors';
     const expected = 2;
     //Act 
     // Call the function you're testing and set the result to a const
     const result = rpsCompair(human, computer);
     //Assert
     assert.equal(result, expected);
+});
+
+test('who wins??', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    let human = 0;
+    let computer = 'scissors';
+    const expected1 = 1;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = rpsCompair(human, computer);
+    //Assert
+    assert.equal(result, expected1);
+});
+
+test('who wins???', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    let human = 2;
+    let computer = 'scissors';
+    const expected2 = 0;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = rpsCompair(human, computer);
+    //Assert
+    assert.equal(result, expected2);
 });
