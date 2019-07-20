075b7aa2cf6ea5f74c33b488fc2e4a22ce75a7ed
Maeve Griffin <griffinmaeve@gmail.com>
2019-07-20 12:34:24 -0700

    now has all three test in testsNumber.test.js

--- test/testsNumber.test.js
+++ test/testsNumber.test.js
@@ -2,7 +2,7 @@ import numberGame from '../src/numberGame.js';
 
 const test = QUnit.test;
 
-test('compair numbers', function(assert) {
+test('correct number', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const guess = 6;
@@ -15,4 +15,29 @@ test('compair numbers', function(assert) {
     assert.equal(result, expected);
 });
 
+test('too low', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const guess = 4;
+    const correct = 6;
+    const expected = -1;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = numberGame(guess, correct);
+    //Assert
+    assert.equal(result, expected);
+});
+
+test('too high', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const guess = 8;
+    const correct = 6;
+    const expected = 1;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = numberGame(guess, correct);
+    //Assert
+    assert.equal(result, expected);
+});
 
